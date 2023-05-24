42d93e26268e82f32f1f8594ae643dda76acb907
Simona Cartolina <simonacartolina@hotmail.com>
2023-05-24 16:55:07 +0200

    creating 16 random numbers

--- myscript.js
+++ myscript.js
@@ -1,9 +1,6 @@
 const playButton=document.querySelector('button');
 const gridElement= document.querySelector('div.grid');
 
-
-
-
 function createElement(tagName, className){
     const cellElement=document.createElement(tagName);
     cellElement.className+= className;
@@ -45,5 +42,36 @@ function createElement(tagName, className){
                             gridElement.appendChild(actualCell); 
                     }
                 });
+
+
+function uniqueNumber(min, max, elements){
+    const numberList=[];
+
+    if((max-min)<elements){
+        return[];
+    }
+
+    while(numberList.length<numberList){
+        const randomNumber= Math.floor(Math.random() * (16 - 1 + 1));
+        if(!numberList.includes(getNewRandomNumber)){
+            numberList.push(getNewRandomNumber);
+        }
+    }
+    return numberList;
+};
+console.log(uniqueNumber);
+
+for(i=0; i<16; i++){
+    uniqueNumber(1, 16, 16);
+};
+
+
+
+/*In seguito l'utente clicca su una cella:
+ se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+  Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+  
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.*/
 
     
